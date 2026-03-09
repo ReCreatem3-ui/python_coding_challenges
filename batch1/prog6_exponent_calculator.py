6d08d45115a5ce34859d814779bc079a3d70df1a
ReCreatem3 <jaysondomanico@gmail.com>
2026-03-09 21:30:47 +0800

    fixed the proper placement of set_number_limit function

--- batch1/prog6_exponent_calculator.py
+++ batch1/prog6_exponent_calculator.py
@@ -9,20 +9,16 @@ while True:
         spacer()
         num1 = int(input("Enter 1st number: "))
         num2 = int(input("Enter 2nd number: "))
-        
-        number_sum = num1 ** num2
-        
         set_number_limit = 100
         if abs(num1) > set_number_limit or abs(num2) > set_number_limit:
             raise OverflowError
         
+        number_sum = num1 ** num2
+        
         spacer()
         print(f"The exponent of {num1} and {num2} is {number_sum}") 
-        spacer()
         break
     except ValueError:
-        spacer()
         print("Invalid Input! Please enter numerical value")
     except OverflowError:
-        spacer()
         print("The Value is too large to compute! Please enter smaller numbers.")
