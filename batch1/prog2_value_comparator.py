f473cf287390b0739b845227fbf96420fa390577
ReCreatem3 <jaysondomanico@gmail.com>
2026-03-09 20:41:46 +0800

    prog2_value_comparator is finished

---
+++ batch1/prog2_value_comparator.py
@@ -0,0 +1,19 @@
+"""Prints Equal when the two numbers were equal"""
+
+def spacer():
+    for i in range (20):
+        print("")
+
+while True:
+    try:
+        spacer()
+        num1 = int(input("Enter 1st number: "))
+        num2 = int(input("Enter 2st number: "))
+        if num1 == num2:
+            spacer()
+            print("Equal")
+        else:
+            spacer()
+        break
+    except ValueError:
+        print("Invalid Input! Your OS has been comprimized")
