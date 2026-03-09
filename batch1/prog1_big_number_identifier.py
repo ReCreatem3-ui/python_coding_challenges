ffde5a0e947e6fc3aaefbe5901a38a92b1898ecc
ReCreatem3 <jaysondomanico@gmail.com>
2026-03-09 20:29:00 +0800

    prog1_big_number_identifier is finished

---
+++ batch1/prog1_big_number_identifier.py
@@ -0,0 +1,19 @@
+"""Indentifies the number with higher value"""
+def spacer():
+    for i in range (20):
+        print("")
+
+while True:
+    try:
+        spacer()
+        num1 = int(input("Enter 1st number: "))
+        num2 = int(input("Enter 1st number: "))
+        if num1 > num2:
+            spacer()
+            print(f"{num1} is bigger than {num2}")
+        else:
+            spacer()
+            print(f"{num2} is bigger than {num1}")
+        break
+    except ValueError:
+        print("Invalid Input! Your OS has been comprimized")
