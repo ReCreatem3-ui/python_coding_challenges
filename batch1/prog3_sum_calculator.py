a37f72e1e8ce8a1474227cc6889c6507cd439200
ReCreatem3 <jaysondomanico@gmail.com>
2026-03-09 20:49:04 +0800

    prog3_sum_calculator.py is finished

---
+++ batch1/prog3_sum_calculator.py
@@ -0,0 +1,18 @@
+"""Calculates the sum of the two numbers"""
+def spacer():
+    for i in range (20):
+        print("")
+
+while True:
+    try:
+        spacer()
+        num1 = int(input("Enter 1st number: "))
+        num2 = int(input("Enter 2nd number: "))
+        
+        number_sum = num1 + num2
+        
+        spacer()
+        print(f"The sum of {num1} and {num2} is {number_sum}")
+        break
+    except ValueError:
+        print("Invalid Input! Your OS has been comprimized")
