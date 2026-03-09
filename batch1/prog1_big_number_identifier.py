a85035b5657438d58054ab16e9e67d08f08c992f
ReCreatem3 <jaysondomanico@gmail.com>
2026-03-09 20:34:37 +0800

    fixed the incorrect comment on num2

--- batch1/prog1_big_number_identifier.py
+++ batch1/prog1_big_number_identifier.py
@@ -7,7 +7,7 @@ while True:
     try:
         spacer()
         num1 = int(input("Enter 1st number: "))
-        num2 = int(input("Enter 1st number: "))
+        num2 = int(input("Enter 2nd number: "))
         if num1 > num2:
             spacer()
             print(f"{num1} is bigger than {num2}")
