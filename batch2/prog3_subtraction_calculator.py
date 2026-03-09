f5faf37e4edc03b8257adb11e797882476bb6ff5
ReCreatem3 <jaysondomanico@gmail.com>
2026-03-09 22:32:09 +0800

    prog3_subtraction_calculator.py is finished

---
+++ batch2/prog3_subtraction_calculator.py
@@ -0,0 +1,18 @@
+"""Calculates the difference between the two numbers"""
+def spacer():
+    for i in range (20):
+        print("")
+
+while True:
+    try:
+        spacer()
+        num1 = int(input("Enter 1st number: "))
+        num2 = int(input("Enter 2nd number: "))
+        
+        number_sum = num1 - num2
+        
+        spacer()
+        print(f"The difference between {num1} and {num2} is {number_sum}")
+        break
+    except ValueError:
+        print("Invalid Input! Please enter numerical value")
