c57d8a19652b1c1d8da578e6e1c053b25d11ae3b
ReCreatem3 <jaysondomanico@gmail.com>
2026-03-09 22:39:57 +0800

    prog4_quotioent_calculator_without_decimal.py is finished

---
+++ batch2/prog4_quotioent_calculator_without_decimal.py
@@ -0,0 +1,19 @@
+"""Calculates the quotient between the two numbers without decimal point"""
+
+def spacer():
+    for i in range (20):
+        print("")
+
+while True:
+    try:
+        spacer()
+        num1 = int(input("Enter 1st number: "))
+        num2 = int(input("Enter 2nd number: "))
+        
+        number_sum = num1 // num2
+        
+        spacer()
+        print(f"The quotient of {num1} and {num2} is {number_sum}")
+        break
+    except ValueError:
+        print("Invalid Input! Please enter numerical value")
